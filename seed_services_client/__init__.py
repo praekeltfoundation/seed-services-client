bb363d8961dce3d17a291bb33bfdfe501721daec
Mike Jones <mike@westerncapelabs.com>
2016-08-03 21:21:04 +0200

    Add service reporting and user token commands and tests

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -3,9 +3,11 @@
 from .identity_store import IdentityStoreApiClient
 from .stage_based_messaging import StageBasedMessagingApiClient
 from .auth import AuthApiClient
+from .control_interface import ControlInterfaceApiClient
 
 __version__ = "0.1.0"
 
 __all__ = [
-    'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient'
+    'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
+    'ControlInterfaceApiClient'
 ]
