71dc985b91bf1bf5153b6c792f4104374f9aab7c
Mike Jones <mike@westerncapelabs.com>
2016-08-03 20:29:29 +0200

    Add auth client

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -2,9 +2,10 @@
 
 from .identity_store import IdentityStoreApiClient
 from .stage_based_messaging import StageBasedMessagingApiClient
+from .auth import AuthApiClient
 
 __version__ = "0.1.0"
 
 __all__ = [
-    'IdentityStoreApiClient', 'StageBasedMessagingApiClient'
+    'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient'
 ]
