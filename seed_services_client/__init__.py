413162b871e119704df138509b62c1865df73e44
Mike Jones <mike@westerncapelabs.com>
2016-08-17 15:24:13 +0200

    Bump version

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -6,7 +6,7 @@ from .auth import AuthApiClient
 from .control_interface import ControlInterfaceApiClient
 from .hub import HubApiClient
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
