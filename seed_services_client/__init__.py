51e852bab026162ceb727ac70180bdd1f620c37e
Mike Jones <mike@westerncapelabs.com>
2016-08-18 14:24:30 +0200

    Bump to version 0.4.0 - thanks @gsvr

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -5,10 +5,11 @@ from .stage_based_messaging import StageBasedMessagingApiClient
 from .auth import AuthApiClient
 from .control_interface import ControlInterfaceApiClient
 from .hub import HubApiClient
+from .message_sender import MessageSenderApiClient
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
-    'ControlInterfaceApiClient', 'HubApiClient'
+    'ControlInterfaceApiClient', 'HubApiClient', 'MessageSenderApiClient'
 ]
