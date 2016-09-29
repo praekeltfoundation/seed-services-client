4449c141ddb829a86d257e9a77815037c9597ebd
Simon de Haan <simon@praekeltfoundation.org>
2016-09-30 00:26:26 +0200

    move scheduler from sbm to services client package

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -6,10 +6,12 @@ from .auth import AuthApiClient
 from .control_interface import ControlInterfaceApiClient
 from .hub import HubApiClient
 from .message_sender import MessageSenderApiClient
+from .scheduler import SchedulerApiClient
 
 __version__ = "0.8.0"
 
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
-    'ControlInterfaceApiClient', 'HubApiClient', 'MessageSenderApiClient'
+    'ControlInterfaceApiClient', 'HubApiClient', 'MessageSenderApiClient',
+    'SchedulerApiClient',
 ]
