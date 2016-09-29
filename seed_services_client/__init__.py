0f5cc841cff058bfe4968114f1a1d48bb9a39610
Simon de Haan <simon@praekeltfoundation.org>
2016-09-30 00:41:10 +0200

    version bump

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -8,7 +8,7 @@ from .hub import HubApiClient
 from .message_sender import MessageSenderApiClient
 from .scheduler import SchedulerApiClient
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
