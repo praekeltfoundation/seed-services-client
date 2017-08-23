e2f9b1bfc2abe4a5d82b6ad64a139669c8461469
Alex Muller <alex@praekelt.org>
2017-08-23 12:10:36 +0100

    Add timeout option to client
    There are some places where we make use of varying timeouts when
    making HTTP requests to Seed services.
    
    There's no way to timeout a session - the requests devs are very
    sure this isn't something they want to add because semantically
    a session can't be timed out.
    
    The recommend way to do this is to subclass HTTPAdapter. I've pinned
    a specific version of requests as we now depend on an internal bit of
    it.

--- seed_services_client/seed_services.py
+++ seed_services_client/seed_services.py
@@ -2,6 +2,20 @@ from demands import JSONServiceClient
 from requests.adapters import HTTPAdapter
 
 
+class SeedHTTPAdapter(HTTPAdapter):
+    """
+    HTTPAdapter child class to implement global timeouts
+    """
+
+    def __init__(self, timeout=None, *args, **kwargs):
+        self.timeout = timeout
+        super(SeedHTTPAdapter, self).__init__(*args, **kwargs)
+
+    def send(self, *args, **kwargs):
+        kwargs['timeout'] = self.timeout
+        return super(SeedHTTPAdapter, self).send(*args, **kwargs)
+
+
 class SeedServicesApiClient(object):
     """
     Base API client for seed services.
@@ -18,17 +32,29 @@ class SeedServicesApiClient(object):
     :param retries:
         (optional) The number of times to retry an HTTP request
 
+    :param timeout:
+        (optional) The number of times to retry an HTTP request
+
     """
 
-    def __init__(self, auth_token, api_url, session=None, retries=0):
+    def __init__(self, auth_token, api_url, session=None, retries=0,
+                 timeout=None):
+
         headers = {'Authorization': 'Token ' + auth_token}
         if session is None:
             session = JSONServiceClient(url=api_url,
                                         headers=headers)
         self.session = session
 
+        http_adapter_kwargs = {}
+
         if retries > 0:
-            http = HTTPAdapter(max_retries=retries)
-            https = HTTPAdapter(max_retries=retries)
-            self.session.mount('http://', http)
-            self.session.mount('https://', https)
+            http_adapter_kwargs['max_retries'] = retries
+
+        if timeout is not None:
+            http_adapter_kwargs['timeout'] = timeout
+
+        http = SeedHTTPAdapter(**http_adapter_kwargs)
+        https = SeedHTTPAdapter(**http_adapter_kwargs)
+        self.session.mount('http://', http)
+        self.session.mount('https://', https)
