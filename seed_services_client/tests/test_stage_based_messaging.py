df93357baede230d00d215a8cd66a535bd624b6c
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-09-03 13:53:38 +0200

    Add paginated response handling for stage based messaging service

--- seed_services_client/tests/test_stage_based_messaging.py
+++ seed_services_client/tests/test_stage_based_messaging.py
@@ -12,10 +12,9 @@ class TestStageBasedMessagingClient(TestCase):
             "NO", "http://sbm.example.org/api/v1")
 
     @responses.activate
-    def test_get_schedules(self):
+    def test_get_schedules_one_page(self):
         # Setup
         response = {
-            "count": 2,
             "next": None,
             "previous": None,
             "results": [
@@ -36,14 +35,61 @@ class TestStageBasedMessagingClient(TestCase):
         # Execute
         result = self.api.get_schedules()
         # Check
-        self.assertEqual(result["count"], 2)
-        self.assertEqual(result["results"][0]["id"], 1)
-        self.assertEqual(result["results"][0]["day_of_week"], "1")
-        self.assertEqual(result["results"][1]["id"], 2)
-        self.assertEqual(result["results"][1]["day_of_week"], "3")
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["day_of_week"], "1")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["day_of_week"], "3")
         self.assertEqual(len(responses.calls), 1)
         self.assertEqual(responses.calls[0].request.url,
                          "http://sbm.example.org/api/v1/schedule/")
+
+    @responses.activate
+    def test_get_schedules_multiple_pages(self):
+        # Setup
+        response = {
+            "next": "http://sbm.example.org/api/v1/schedule/?cursor=1",
+            "previous": None,
+            "results": [
+                {
+                    "id": 1, "minute": "0", "hour": "8", "day_of_week": "1",
+                    "day_of_month": "*", "month_of_year": "*"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/schedule/",
+                      json=response, status=200,
+                      match_querystring=True)
+        response = {
+            "next": None,
+            "previous": "http://sbm.example.org/api/v1/schedule/?cursor=0",
+            "results": [
+                {
+                    "id": 2, "minute": "0", "hour": "8", "day_of_week": "3",
+                    "day_of_month": "*", "month_of_year": "*"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/schedule/?cursor=1",
+                      json=response, status=200,
+                      match_querystring=True)
+        # Execute
+        result = self.api.get_schedules()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["day_of_week"], "1")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["day_of_week"], "3")
+        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(responses.calls[0].request.url,
+                         "http://sbm.example.org/api/v1/schedule/")
+        self.assertEqual(responses.calls[1].request.url,
+                         "http://sbm.example.org/api/v1/schedule/?cursor=1")
 
     @responses.activate
     def test_get_schedule(self):
@@ -94,6 +140,106 @@ class TestStageBasedMessagingClient(TestCase):
                          "http://sbm.example.org/api/v1/messageset/11/")
 
     @responses.activate
+    def test_list_messagesets_one_page(self):
+        # Setup
+        response = {
+            "previous": None,
+            "next": None,
+            "results": [{
+                'id': 1,
+                'short_name': "short_name_1",
+                'notes': None,
+                'next_set': 10,
+                'default_schedule': 1,
+                'content_type': 'text',
+                'created_at': "2016-06-22T06:13:29.693272Z",
+                'updated_at': "2016-06-22T06:13:29.693272Z"
+            }, {
+                'id': 2,
+                'short_name': "short_name_2",
+                'notes': None,
+                'next_set': 10,
+                'default_schedule': 1,
+                'content_type': 'text',
+                'created_at': "2016-06-22T06:13:29.693272Z",
+                'updated_at': "2016-06-22T06:13:29.693272Z"
+            }]
+        }
+        responses.add(
+            responses.GET,
+            "http://sbm.example.org/api/v1/messageset/",
+            json=response, status=200, match_querystring=True
+        )
+        # Execute
+        result = self.api.get_messagesets()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result1["short_name"], "short_name_1")
+        self.assertEqual(result2["short_name"], "short_name_2")
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url,
+                         "http://sbm.example.org/api/v1/messageset/")
+
+    @responses.activate
+    def test_list_messagesets_multiple_pages(self):
+        # Setup
+        response = {
+            "previous": None,
+            "next": "http://sbm.example.org/api/v1/messageset/?cursor=1",
+            "results": [{
+                'id': 1,
+                'short_name': "short_name_1",
+                'notes': None,
+                'next_set': 10,
+                'default_schedule': 1,
+                'content_type': 'text',
+                'created_at': "2016-06-22T06:13:29.693272Z",
+                'updated_at': "2016-06-22T06:13:29.693272Z"
+            }]
+        }
+        responses.add(
+            responses.GET,
+            "http://sbm.example.org/api/v1/messageset/",
+            json=response, status=200, match_querystring=True
+        )
+        response = {
+            "previous": "http://sbm.example.org/api/v1/messageset/cursor=0",
+            "next": None,
+            "results": [{
+                'id': 2,
+                'short_name': "short_name_2",
+                'notes': None,
+                'next_set': 10,
+                'default_schedule': 1,
+                'content_type': 'text',
+                'created_at': "2016-06-22T06:13:29.693272Z",
+                'updated_at': "2016-06-22T06:13:29.693272Z"
+            }]
+        }
+        responses.add(
+            responses.GET,
+            "http://sbm.example.org/api/v1/messageset/?cursor=1",
+            json=response, status=200, match_querystring=True
+        )
+        # Execute
+        result = self.api.get_messagesets()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result1["short_name"], "short_name_1")
+        self.assertEqual(result2["short_name"], "short_name_2")
+        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(responses.calls[0].request.url,
+                         "http://sbm.example.org/api/v1/messageset/")
+        self.assertEqual(responses.calls[1].request.url,
+                         "http://sbm.example.org/api/v1/messageset/?cursor=1")
+
+    @responses.activate
     def test_messageset_languages(self):
         # Setup
         data = {
@@ -118,13 +264,12 @@ class TestStageBasedMessagingClient(TestCase):
                          "http://sbm.example.org/api/v1/messageset_languages/")
 
     @responses.activate
-    def test_get_subscriptions(self):
+    def test_get_subscriptions_one_page(self):
         # Setup
         registrant_id = "4275a063-3129-45ac-853b-0d64aaefd8c5"
         subscription_id_1 = "subscription1-4bf1-8779-c47b428e89d0"
         subscription_id_2 = "subscription2-4bf1-8779-c47b428e89d0"
         response = {
-            "count": 2,
             "next": None,
             "previous": None,
             "results": [
@@ -171,12 +316,85 @@ class TestStageBasedMessagingClient(TestCase):
         # Execute
         result = self.api.get_subscriptions()
         # Check
-        self.assertEqual(result["count"], 2)
-        self.assertEqual(result["results"][0]["id"], subscription_id_1)
-        self.assertEqual(result["results"][0]["active"], True)
-        self.assertEqual(result["results"][1]["id"], subscription_id_2)
-        self.assertEqual(result["results"][1]["active"], True)
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], subscription_id_1)
+        self.assertEqual(result1["active"], True)
+        self.assertEqual(result2["id"], subscription_id_2)
+        self.assertEqual(result2["active"], True)
         self.assertEqual(len(responses.calls), 1)
+
+    @responses.activate
+    def test_get_subscriptions_multiple_pages(self):
+        # Setup
+        registrant_id = "4275a063-3129-45ac-853b-0d64aaefd8c5"
+        subscription_id_1 = "subscription1-4bf1-8779-c47b428e89d0"
+        subscription_id_2 = "subscription2-4bf1-8779-c47b428e89d0"
+        response = {
+            "next": "http://sbm.example.org/api/v1/subscriptions/?cursor=1",
+            "previous": None,
+            "results": [
+                {
+                    "id": subscription_id_1,
+                    "identity": registrant_id,
+                    "active": True,
+                    "completed": False,
+                    "lang": "eng_ZA",
+                    "url": "http://sbm/api/v1/subscriptions/%s" % (
+                        subscription_id_1),
+                    "messageset": 11,
+                    "next_sequence_number": 11,
+                    "schedule": 101,
+                    "process_status": 0,
+                    "version": 1,
+                    "metadata": {},
+                    "created_at": "2015-07-10T06:13:29.693272Z",
+                    "updated_at": "2015-07-10T06:13:29.693272Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/subscriptions/",
+                      json=response, status=200,
+                      match_querystring=True)
+        response = {
+            "next": None,
+            "previous": "http://sbm.example.org/api/v1/subscriptions/?"
+                        "cursor=0",
+            "results": [
+                {
+                    "id": subscription_id_2,
+                    "identity": registrant_id,
+                    "active": True,
+                    "completed": False,
+                    "lang": "eng_ZA",
+                    "url": "http://sbm/api/v1/subscriptions/%s" % (
+                        subscription_id_2),
+                    "messageset": 21,
+                    "next_sequence_number": 21,
+                    "schedule": 121,
+                    "process_status": 0,
+                    "version": 1,
+                    "metadata": {},
+                    "created_at": "2015-07-10T06:13:29.693272Z",
+                    "updated_at": "2015-07-10T06:13:29.693272Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/subscriptions/?cursor=1",
+                      json=response, status=200,
+                      match_querystring=True)
+        # Execute
+        result = self.api.get_subscriptions()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], subscription_id_1)
+        self.assertEqual(result1["active"], True)
+        self.assertEqual(result2["id"], subscription_id_2)
+        self.assertEqual(result2["active"], True)
+        self.assertEqual(len(responses.calls), 2)
 
     @responses.activate
     def test_get_subscription(self):
@@ -290,3 +508,102 @@ class TestStageBasedMessagingClient(TestCase):
         self.assertEqual(len(responses.calls), 1)
         self.assertEqual(responses.calls[0].request.url,
                          "http://sbm.example.org/api/v1/subscriptions/")
+
+    @responses.activate
+    def test_get_messages_one_page(self):
+        # Setup
+        response = {
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 1 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                },
+                {
+                    "id": 2,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 2 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/",
+                      json=response, status=200,
+                      match_querystring=True)
+
+        # Execute
+        result = self.api.get_messages()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["text_content"], "message 1 content")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["text_content"], "message 2 content")
+        self.assertEqual(len(responses.calls), 1)
+
+    @responses.activate
+    def test_get_messages_multiple_pages(self):
+        # Setup
+        response = {
+            "next": "http://sbm.example.org/api/v1/message/?cursor=1",
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 1 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/",
+                      json=response, status=200,
+                      match_querystring=True)
+        response = {
+            "next": None,
+            "previous": "http://sbm.example.org/api/v1/message/?cursor=0",
+            "results": [
+                {
+                    "id": 2,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 2 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/?cursor=1",
+                      json=response, status=200,
+                      match_querystring=True)
+        # Execute
+        result = self.api.get_messages()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["text_content"], "message 1 content")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["text_content"], "message 2 content")
+        self.assertEqual(len(responses.calls), 2)
