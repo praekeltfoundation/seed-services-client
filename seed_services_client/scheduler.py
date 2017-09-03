7fef1a1bfb78324dc54e684a26991cb4c4722102
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-09-03 14:19:51 +0200

    SchedulerApiClient subclasses SeedServicesApiClient

--- seed_services_client/scheduler.py
+++ seed_services_client/scheduler.py
@@ -3,74 +3,42 @@
 Client for Messaging Content Store HTTP services APIs.
 
 """
-import requests
-import json
+from .seed_services import SeedServicesApiClient
 
 
-class SchedulerApiClient(object):
-
+class SchedulerApiClient(SeedServicesApiClient):
     """
-    Client for Scheduler API.
+    Client for Scheduler Service.
 
-    :param str api_token:
-
-        An API Token.
+    :param str auth_token:
+        An access token.
 
     :param str api_url:
-        The full URL of the API. Defaults to
-        ``http://seed-scheduler/api/v1``.
+        The full URL of the API.
+
+    :param JSONServiceClient session:
+        An instance of JSONServiceClient to use
 
     """
 
-    def __init__(self, api_token, api_url=None, session=None):
-        if api_url is None:
-            api_url = "http://seed-scheduler/api/v1"
-        self.api_url = api_url
-        self.headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token %s' % api_token
-        }
-        if session is None:
-            session = requests.Session()
-        session.headers.update(self.headers)
-        self.session = session
-
-    def call(self, endpoint, method, obj=None, params=None, data=None):
-        if obj is None:
-            url = '%s/%s/' % (self.api_url.rstrip('/'), endpoint)
-        else:
-            url = '%s/%s/%s/' % (self.api_url.rstrip('/'), endpoint, obj)
-        result = {
-            'get': self.session.get,
-            'post': self.session.post,
-            'patch': self.session.patch,
-            'delete': self.session.delete,
-        }.get(method, None)(url, params=params, data=json.dumps(data))
-        result.raise_for_status()
-        if method is "delete":  # DELETE returns blank body
-            return {"success": True}
-        else:
-            return result.json()
-
     def get_schedules(self, params=None):
-        return self.call('schedule', 'get', params=params)
+        return self.session.get('/schedule/', params=params)
 
     def get_schedule(self, schedule_id):
-        return self.call('schedule', 'get', obj=schedule_id)
+        return self.session.get('/schedule/%s/' % schedule_id)
 
     def create_schedule(self, schedule):
-        return self.call('schedule', 'post', data=schedule)
+        return self.session.post('/schedule/', data=schedule)
 
     def update_schedule(self, schedule_id, schedule):
-        return self.call('schedule', 'patch', obj=schedule_id,
-                         data=schedule)
+        return self.session.patch('/schedule/%s/' % schedule_id,
+                                  data=schedule)
 
     def delete_schedule(self, schedule_id):
-        # Schedule messages must all be deleted first for FK reasons
-        return self.call('schedule', 'delete', obj=schedule_id)
+        return self.session.delete('/schedule/%s/' % schedule_id)
 
     def get_failed_tasks(self, params=None):
-        return self.call('failed-tasks', 'get', params=params)
+        return self.session.get('/failed-tasks/', params=params)
 
     def requeue_failed_tasks(self):
-        return self.call('failed-tasks', 'post', data={})
+        return self.session.post('/failed-tasks/')
