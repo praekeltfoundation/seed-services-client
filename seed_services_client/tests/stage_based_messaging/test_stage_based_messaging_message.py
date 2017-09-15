3dc49f820c52bef78ceb57240fd7db1cc0d3ae07
Alex Muller <alex@praekelt.org>
2017-09-15 12:35:31 +0100

    Split stage_based_messaging tests into files

---
+++ seed_services_client/tests/stage_based_messaging/test_stage_based_messaging_message.py
@@ -0,0 +1,110 @@
+import responses
+
+from seed_services_client import StageBasedMessagingApiClient
+from unittest import TestCase
+
+
+class TestStageBasedMessagingClientMessage(TestCase):
+
+    def setUp(self):
+        self.api = StageBasedMessagingApiClient(
+            "token", "http://sbm.example.org/api/v1")
+
+    @responses.activate
+    def test_get_messages_one_page(self):
+        # Setup
+        response = {
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 1 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                },
+                {
+                    "id": 2,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 2 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/",
+                      json=response, status=200,
+                      match_querystring=True)
+
+        # Execute
+        result = self.api.get_messages()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["text_content"], "message 1 content")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["text_content"], "message 2 content")
+        self.assertEqual(len(responses.calls), 1)
+
+    @responses.activate
+    def test_get_messages_multiple_pages(self):
+        # Setup
+        response = {
+            "next": "http://sbm.example.org/api/v1/message/?cursor=1",
+            "previous": None,
+            "results": [
+                {
+                    "id": 1,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 1 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/",
+                      json=response, status=200,
+                      match_querystring=True)
+        response = {
+            "next": None,
+            "previous": "http://sbm.example.org/api/v1/message/?cursor=0",
+            "results": [
+                {
+                    "id": 2,
+                    "messageset": 1,
+                    "sequence_number": 1,
+                    "lang": "eng_ZA",
+                    "text_content": "message 2 content",
+                    "binary_content": None,
+                    "created_at": "2016-09-23T14:12:09.876010Z",
+                    "updated_at": "2016-09-23T14:12:09.876036Z"
+                }
+            ]
+        }
+        responses.add(responses.GET,
+                      "http://sbm.example.org/api/v1/message/?cursor=1",
+                      json=response, status=200,
+                      match_querystring=True)
+        # Execute
+        result = self.api.get_messages()
+        # Check
+        result1 = next(result["results"])
+        result2 = next(result["results"])
+        self.assertEqual(result1["id"], 1)
+        self.assertEqual(result1["text_content"], "message 1 content")
+        self.assertEqual(result2["id"], 2)
+        self.assertEqual(result2["text_content"], "message 2 content")
+        self.assertEqual(len(responses.calls), 2)
