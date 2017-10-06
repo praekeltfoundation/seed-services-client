4f51fa3324c38a49c325194a14d1bdba28187a34
Alex Muller <alex@praekelt.org>
2017-10-06 17:31:37 +0100

    Add User-Agent HTTP header
    This will make it easier to debug in the application logs.
    
    I can't figure out a way to patch the version number to test the
    entire header.

--- seed_services_client/__init__.py
+++ seed_services_client/__init__.py
@@ -9,7 +9,7 @@ from .message_sender import MessageSenderApiClient
 from .scheduler import SchedulerApiClient
 from .service_rating import ServiceRatingApiClient
 
-__version__ = "0.31.0"
+from .__version__ import __version__  # noqa: F401
 
 __all__ = [
     'IdentityStoreApiClient', 'StageBasedMessagingApiClient', 'AuthApiClient',
